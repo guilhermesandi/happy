72592aece19774b0e4f3179285f6b0f03dded174
Guilherme Sandi <guilherme_sandi@hotmail.com>
2020-10-20 00:42:46 -0300

    Function button form

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -67,3 +67,21 @@ function deleteField(event) {
     // deletar o campo
     span.parentNode.remove();
 }
+
+// seleção sim e não
+function toggleSelect(event) {
+    // retirar a class .active (dos botões)
+    document.querySelectorAll('.button-select button')
+    .forEach( function(button) {
+        button.classList.remove('active')
+    })
+
+    // colocar a class .active nesse botão clicado
+    const button = event.currentTarget
+    button.classList.add('active')
+
+    // atualizar o meu input hidden com o valor selecionado
+    const input = document.querySelector('[name="open_on_weekends"]')
+    
+    input.value = button.dataset.value
+}
