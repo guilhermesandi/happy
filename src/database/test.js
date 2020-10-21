94be2dd22e434b70deb4a200595e1bad1821baea
Guilherme Sandi <guilherme_sandi@hotmail.com>
2020-10-21 12:14:38 -0300

    Configure database

--- src/database/test.js
+++ src/database/test.js
@@ -1,36 +1,32 @@
 const Database = require('./db');
+const saveOrphanage = require('./saveOrphanage');
 
 Database.then(async db => {
     // inserir dados na tabela
-    await db.run(`
-        INSERT INTO orphanages (
-            lat,
-            lng,
-            name,
-            about,
-            whatsapp,
-            images,
-            instructions,
-            opening_hours,
-            open_on_weekends
-        ) VALUES (
-            "-19.9233136",
-            "-43.9559462",
-            "Lar dos meninos",
-            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
-            "31947642198",
+    await saveOrphanage(db, {
+        lat: "-19.9233136",
+        lng: "-43.9469462",
+        name: "Lar dos meninos",
+        about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        whatsapp: "31984827381",
+        images: [
             "https://images.unsplash.com/photo-1582167371270-68a4c033e732?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-            "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
-            "Horário de visitas Das 18h até 8h",
-            "0"
-        );
-    `)
+
+            "https://images.unsplash.com/photo-1595207011175-3d72f5a3b21c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ].toString(),
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+    })
     
     // consultar dados da tabela
     const selectedOrphanages = await db.all("SELECT * FROM orphanages")
     console.log(selectedOrphanages)
 
-    // consultar somente 1 orfanato, pelo id
+    // // consultar somente 1 orfanato, pelo id
     const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "1"')
     console.log(orphanage)
+
+    // // deletar dado da tabela
+    // console.log(await db.run("DELETE FROM )orphanages WHERE id = '4'"))
 })
